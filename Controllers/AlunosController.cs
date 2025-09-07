ce6ec5e4f573606737d40503705e62900178d41d
Fernando Coelho <fernandocoelho9000@gmail.com>
2025-09-07 20:13:06 -0300

    Controller Alunos Metodo Get All Ok

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,35 @@
+﻿using AlunosApi.Models;
+using AlunosApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosApi.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        private IAlunoService _alunoService;
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        public int Idade { get; set; }
+    }
+}
